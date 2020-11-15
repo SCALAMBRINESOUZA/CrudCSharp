02f7c3835f96922ad81b1ed462bd0437dec80981
SCALAMBRINESOUZA <henrique.joaovictor@hotmail.com>
2020-11-15 02:36:12 -0300

    Inclusao de tres componentes de interface uma label e button e um textbox

--- ProjetoAcme/Form1.cs
+++ ProjetoAcme/Form1.cs
@@ -10,11 +10,29 @@ using System.Windows.Forms;
 
 namespace ProjetoAcme
 {
+
+    
+
     public partial class Form1 : Form
     {
+        string sdi = "HELLO";
+        string sbv = "SEJA BEM VINDO";
+
         public Form1()
         {
             InitializeComponent();
+            MessageBox.Show(sdi);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(sbv);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string nome = textBox1.Text;
+            MessageBox.Show(sdi + " " + sbv + " " + nome);
         }
     }
 }
